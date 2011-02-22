cc382d087a50642e249b2de5d1243dcea016c234
Christian Schiffler <c.schiffler@cyberspectrum.de>
2011-02-22 19:26:49 +0000

    - enhanced TableTree to honor an optional "sortColumn" in "eval" to sort the items within a tree node. - Changed Catalog behaviour to not overwrite the language strings of fields if defined in langconfig.php - expanded parameters of HOOK "getCatalogDca" to also include the catalog dataContainer and the module itself. - made configOptions public - updated tinyFrontend.php to reflect the changes in the contao version.
    git-svn-id: http://typolight-catalog.googlecode.com/svn@226 5f8f583f-444c-0410-842f-6b1beb028a02

--- src/system/config/tinyFrontend.php
+++ src/system/config/tinyFrontend.php
@@ -1,13 +1,15 @@
 <?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
 
 /**
- * TYPOlight webCMS
- * Copyright (C) 2005-2009 Leo Feyer
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
- * version 2.1 of the License, or (at your option) any later version.
+ * version 3 of the License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,11 +18,11 @@
  * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
- * Software Foundation website at http://www.gnu.org/licenses/.
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
  *
  * PHP version 5
- * @copyright  Leo Feyer 2005-2009
- * @author     Leo Feyer <leo@typolight.org>
+ * @copyright  Leo Feyer 2005-2010
+ * @author     Leo Feyer <http://www.contao.org>
  * @package    Config
  * @license    LGPL
  * @filesource
@@ -28,15 +30,15 @@
 
 
 /**
- * This is the tinyMCE (rich text editor) configuration file for flash content.
- * Please visit http://tinymce.moxiecode.com for more information.
+ * This is the tinyMCE (rich text editor) configuration file for the catalog frontend editing. Please visit
+ * http://tinymce.moxiecode.com for more information.
  */
-?>
+if ($GLOBALS['TL_CONFIG']['useRTE']): ?>
 <script type="text/javascript" src="<?php echo $this->base; ?>plugins/tinyMCE/tiny_mce_gzip.js"></script>
 <script type="text/javascript">
 <!--//--><![CDATA[//><!--
 tinyMCE_GZ.init({
-  plugins : "advimage,autosave,contextmenu,directionality,emotions,inlinepopups,paste,safari,save,searchreplace,spellchecker,style,table,template,typolinks,xhtmlxtras",
+  plugins : "advimage,autosave,directionality,emotions,inlinepopups,paste,save,searchreplace,spellchecker,style,tabfocus,table,template,typolinks,xhtmlxtras",
   themes : "advanced",
   languages : "<?php echo $this->language; ?>",
   disk_cache : false,
@@ -60,17 +62,20 @@ tinyMCE.init({
   force_hex_style_colors : true,
   fix_list_elements : true,
   fix_table_elements : true,
-  font_size_style_values : "1,2,3,4,5,6,7",
+  theme_advanced_font_sizes : "9px,10px,11px,12px,13px,14px,15px,16px,17px,18px,19px,20px,21px,22px,23px,24px",
+  doctype : '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
   document_base_url : "<?php echo $this->base; ?>",
-  entities : "160,nbsp,60,lt,62,gt",
+  entities : "160,nbsp,60,lt,62,gt,173,shy",
   cleanup_on_startup : true,
   save_enablewhendirty : true,
   save_on_tinymce_forms : true,
-  plugins : "advimage,autosave,contextmenu,directionality,emotions,inlinepopups,paste,safari,save,searchreplace,spellchecker,style,table,template,typolinks,xhtmlxtras", 
-  spellchecker_languages : "+English=en,Danish=da,Dutch=nl,Finnish=fi,French=fr,German=de,Italian=it,Polish=pl,Portuguese=pt,Spanish=es,Swedish=sv",
-  valid_elements : "+a[href|target|class],-b/strong,-i/em,-u,+div[align],+p[class|align],-li,br,-span[class],font[face|size|color]",
-  content_css : "<?php echo TL_PATH; ?>/basic.css,<?php echo TL_PATH; ?>/system/themes/tinymce.css",
-  tab_focus : ":prev,:next",
+  advimage_update_dimensions_onchange : false,
+  plugins : "advimage,autosave,directionality,emotions,inlinepopups,paste,save,searchreplace,spellchecker,style,tabfocus,table,template,xhtmlxtras",
+  spellchecker_languages : "<?php echo $this->getSpellcheckerString(); ?>",
+  content_css : "<?php echo TL_PATH; ?>/system/themes/tinymce.css,<?php echo TL_PATH .'/'. $this->uploadPath; ?>/tinymce.css",
+  event_elements : "a,div,h1,h2,h3,h4,h5,h6,img,p,span",
+  extended_valid_elements : "q[cite|class|title]",
+  tabfocus_elements : ":prev,:next",
   theme : "advanced",
   theme_advanced_resizing : true,
   theme_advanced_resize_horizontal : false,
@@ -79,9 +84,9 @@ tinyMCE.init({
   theme_advanced_statusbar_location : "bottom",
   theme_advanced_source_editor_width : "700",
   theme_advanced_blockformats : "div,p,address,pre,h1,h2,h3,h4,h5,h6",
-  theme_advanced_buttons1 : "newdocument,separator,spellchecker,separator,anchor,separator,typolinks,unlink,separator,image,charmap,emotions,separator,sub,sup,separator,abbr,acronym,separator,styleprops,attribs,separator,search,replace,pasteword,separator,undo,redo,separator,removeformat,cleanup,separator,code",
-  theme_advanced_buttons2 : "formatselect,fontsizeselect,styleselect,separator,bold,italic,underline,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,outdent,indent,separator,forecolor,backcolor",
-  theme_advanced_buttons3 : "tablecontrols,separator,template"
+  theme_advanced_buttons1 : "newdocument,save,separator,spellchecker,separator,anchor,separator,typolinks,unlink,separator,image,typobox,separator,sub,sup,separator,abbr,acronym,separator,styleprops,attribs,separator,search,replace,separator,undo,redo,separator,removeformat,cleanup,separator,code",
+  theme_advanced_buttons2 : "formatselect,fontsizeselect,styleselect,separator,bold,italic,underline,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,indent,outdent,separator,blockquote,separator,forecolor,backcolor",
+  theme_advanced_buttons3 : "tablecontrols,separator,template,separator,charmap,emotions,separator,help"
 });
 //--><!]]>
 </script>
